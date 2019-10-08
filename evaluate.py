e2ced8065edcc732cd56823c23fb950647d77dd6
lchancri <lchancri@student.42.fr>
2019-10-08 15:32:49 +0200

    Splitted the fle in two

---
+++ evaluate.py
@@ -0,0 +1,62 @@
+def and_function(expression, i):
+    if expression[i-1] == 1 and expression[i+1] == 1:
+        expression[i-1] = 1
+    else:
+        expression[i-1] = -1
+    return expression
+
+def or_function(expression, i):
+    if expression[i-1] == 1 or expression[i+1] == 1:
+        expression[i-1] = 1
+    else:
+        expression[i-1] = -1
+    return expression
+
+def xor_function(expression, i):
+    if ((expression[i-1] == 1 and expression[i+1] == -1)
+            or (expression[i-1] == -1 and expression[i+1] == 1)):
+        expression[i-1] = 1
+    else:
+        expression[i-1] = -1
+    return expression
+
+def calcul(expression, i):
+    if expression[i] == '+':
+        expression = and_function(expression, i)
+    if expression[i] == '|':
+        expression = or_function(expression, i)
+    if expression[i] == '^':
+        expression = xor_function(expression, i)
+    expression.remove(expression[i])
+    expression.remove(expression[i])
+
+    print(expression)
+    return expression
+
+def evaluate_expression(expression, dico, list_of_symbols):
+    expression = list(expression)
+    print(expression)
+    # Remove < because it is useless to evaluate the expression
+    if "<" in expression:
+        expression.remove("<")
+    # Replace characters with value 1 or -1
+    for i in range(0, len(expression)):
+        if expression[i] not in list_of_symbols:
+            expression[i] = dico[expression[i]]
+    # Turn !-1 in 1 and !1 in -1
+    for i in range(0, len(expression)):
+        if expression[i] == '!':
+            expression[i+1] *= -1
+    if "!" in expression:
+        expression.remove("!")
+
+    print(expression)
+    length = len(expression)
+    i = 0
+    while i < length:
+        if expression[i] in list_of_symbols:
+           expression = calcul(expression, i)
+           length -= 2
+           i -= 1
+        i += 1
+    return int(expression[0])
--- expert_system.py
+++ expert_system.py
@@ -1,5 +1,6 @@
 import sys
 import parse
+import evaluate
 
 '''
 () = priority order
@@ -11,72 +12,19 @@ import parse
 <=> if and only if (implies in both way)
 '''
 
-def and_function(expression, i):
-    if expression[i-1] == 1 and expression[i+1] == 1:
-        expression[i-1] = 1
+def change_dict_value(expression, dico, first, if_and_only_if):
+
+    return dico
+
+def evaluate_2(dico, line, list_of_symbols):
+    if "<=>" in line:
+        if_and_only_if = True
     else:
-        expression[i-1] = -1
-    return expression
-
-def or_function(expression, i):
-    if expression[i-1] == 1 or expression[i+1] == 1:
-        expression[i-1] = 1
-    else:
-        expression[i-1] = -1
-    return expression
-
-def xor_function(expression, i):
-    if ((expression[i-1] == 1 and expression[i+1] == -1)
-            or (expression[i-1] == -1 and expression[i+1] == 1)):
-        expression[i-1] = 1
-    else:
-        expression[i-1] = -1
-    return expression
-
-def calcul(expression, i):
-    if expression[i] == '+':
-        expression = and_function(expression, i)
-    if expression[i] == '|':
-        expression = or_function(expression, i)
-    if expression[i] == '^':
-        expression = xor_function(expression, i)
-    expression.remove(expression[i])
-    expression.remove(expression[i])
-
-    print(expression)
-    return expression
-
-def evaluate_1(expression, dico, list_of_symbols):
-    expression = list(expression)
-    print(expression)
-    # Remove < because it is useless to evaluate the expression
-    if "<" in expression:
-        expression.remove("<")
-    # Replace characters with value 1 or -1
-    for i in range(0, len(expression)):
-        if expression[i] not in list_of_symbols:
-            expression[i] = dico[expression[i]]
-    # Turn !-1 in 1 and !1 in -1
-    for i in range(0, len(expression)):
-        if expression[i] == '!':
-            expression[i+1] *= -1
-    if "!" in expression:
-        expression.remove("!")
-
-    print(expression)
-    length = len(expression)
-    i = 0
-    while i < length:
-        if expression[i] in list_of_symbols:
-           expression = calcul(expression, i)
-           length -= 2
-           i -= 1
-        i += 1
-
-def evaluate(dico, line, list_of_symbols):
+        if_and_only_if = False
     expression = line.split("=")
-    first = evaluate_1(expression[0], dico, list_of_symbols)
-#    dico = evaluate_2(expression[1], dico, first)
+    first = evaluate.evaluate_expression(expression[0], dico, list_of_symbols)
+    print(first)
+    dico = change_dict_value(expression[1], dico, first, if_and_only_if)
     print("Expression   :", expression)
     print("Dictionnaire :", dico)
     print("\n")
@@ -88,7 +36,7 @@ def algo(fichier, dico, target, list_of_symbols):
         if line == '' or line[0] in exceptions:
             pass
         else:
-            dico = evaluate(dico, line, list_of_symbols)
+            dico = evaluate_2(dico, line, list_of_symbols)
 
 def main():
     try:
