a4f59240f6254292bd0fdc7b3a8b0cb95af6ea0d
lchancri <lchancri@student.42.fr>
2019-11-27 13:23:47 +0100

    Suppression du code en commentaire

--- moteur.py
+++ moteur.py
@@ -19,52 +19,8 @@ def ft_xor(left, right):
 
 functions = {'!': ft_not, '+': ft_and, '|': ft_or, '^': ft_xor}
 
-# def solve_querie(tree, querie, values, facts):
-# 	check = False
-# 	print(querie)
-# 	print(values)
-# 	print(facts)
-# 	print("")
-# 	for q in facts:
-# 		if check == True:
-# 			if facts[q] == None:
-# 				return None
-# 		if q == querie:
-# 			check = True
-# 	if True in values:
-# 		return True
-# 	return False
-
-# def evaluate(rule, facts, dictionnaire):
-# 	if not rule:
-# 		return facts, None
-# 	if rule.value and rule.value.isalpha():
-# 		if rule.value in facts:
-# 			return facts, facts[rule.value]
-# 		else:
-# 			facts[rule.value] = moteur(rule.value, facts, dictionnaire)
-# 			return facts, facts[rule.value]
-# 	facts, left = evaluate(rule.left, facts, dictionnaire)
-# 	fatcs, right = evaluate(rule.right, facts, dictionnaire)
-# 	return facts, functions[rule.value](left, right)
-
-# def moteur(querie, facts, dictionnaire):
-# 	facts[querie] = False
-# 	if querie not in dictionnaire:
-# 		return False
-# 	values = []
-# 	for rule in dictionnaire[querie]:
-# 		facts, value = evaluate(rule.left, facts, dictionnaire)
-# 		values.append(value)
-# 	return solve_querie(rule.right, querie, values, facts)
-
-
 def solve_querie(querie, values, facts):
 	check = False
-	#print(querie)
-	#print(values)
-	#print(facts)
-	#print("")
 	for q in facts:
 		if check == True:
 			if facts[q] == None:
